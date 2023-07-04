cf92b4f8dd825c6437a6b2b535a2b7ad9112c618
Vo Nguyen <progaptrum8@gmail.com>
2023-07-04 09:31:09 +0700

    login

--- web-view/src/components/auth/login.tsx
+++ web-view/src/components/auth/login.tsx
@@ -1,7 +1,54 @@
+"use client";
+
 import Head from "next/head";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import "@/src/app/globals.css";
+import { MessageError } from "../commons/MessageError";
+import { useEffect, useState } from "react";
+
+export type FormDataLogin = {
+  email: string;
+  password: string;
+  // name?: string;
+  // googleId?: string;
+  // facebookId?: string;
+  // imageUrl?: string;
+};
+
+const schema = yup.object().shape({
+  email: yup.string().required("Email is required").email("Email is not valid"),
+  password: yup.string().required("Password is required"),
+});
+
 const Login = () => {
+  const [errorMessageEmail, setErrorMessageEmail] = useState<string | null>(null);
+  const [errorMessagePwd, setErrorMessagePwd] = useState<string | null>(null);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormDataLogin>({
+    resolver: yupResolver(schema),
+  });
+
+  const emailValue = watch("email");
+  const passwordValue = watch("password");
+
+  useEffect(() => {
+    setErrorMessageEmail(null);
+    setErrorMessagePwd(null);
+  }, [emailValue, passwordValue]);
+
+  const onSubmit = (data: FormDataLogin) => {
+    console.log(data);
+    // setUser((pre) => ({ ...pre, loading: true, error: null, success: false }));
+    // userLogin(data, () => navigate(0));
+  };
+
   return (
     <>
       <Head>
@@ -22,21 +69,25 @@ const Login = () => {
           </Link>
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Sign in to your account
               </h1>
-              <form className="space-y-4 md:space-y-6" action="#">
+              <form
+                className="space-y-4 md:space-y-6"
+                onSubmit={handleSubmit(onSubmit)}
+              >
                 <div>
                   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                     Your email
                   </label>
                   <input
                     type="text"
-                    name="username"
-                    id="username"
-                    placeholder="Username"
+                    {...register("email")}
+                    placeholder="Email"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   />
+                  {errors.email && <MessageError message={errors.email.message} />}
+                  {errorMessageEmail && <MessageError message={errorMessageEmail} />}
                 </div>
                 <div>
                   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -44,11 +95,11 @@ const Login = () => {
                   </label>
                   <input
                     type="password"
-                    name="password"
-                    id="password"
-                    placeholder="••••••••"
+                    {...register("password")}
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   />
+                  {errors.password && <MessageError message={errors.password.message} />}
+                  {errorMessagePwd && <MessageError message={errorMessagePwd} />}
                 </div>
                 <div className="flex items-center justify-between">
                   <div className="flex items-start">
@@ -79,7 +130,7 @@ const Login = () => {
                 >
                   Sign in
                 </button>
-                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                <p className="text-sm text-center font-light text-gray-500 dark:text-gray-400">
                   Don’t have an account yet?{" "}
                   <a
                     href="#"
